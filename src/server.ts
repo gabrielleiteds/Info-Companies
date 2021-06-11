17a0a273671497fae4b608960bbbe2a6e2844e11
leitegs <leitedasilvag@gmail.com>
2021-06-11 19:38:23 -0300

    feat: implementing server to started

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import path from 'path'
 import * as dotenv from 'dotenv'
+import app from './api'
+import MongoConnection from './infra/helpers/database/mongo-connection'
 
 const environment = process.env.NODE_ENV || 'localhost'
 const envPath = path.join(__dirname, '..', 'env', environment + '.env')
 
 dotenv.config({ path: envPath })
+
+MongoConnection.connect()
+
+const port = process.env.PORT || 3333
+
+app.listen(port, () => {
+  console.log(`Server ${process.env.NODE_ENV} ready on PORT ${port}`)
+})
