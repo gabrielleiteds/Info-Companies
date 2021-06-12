ff5db2e48550bbece6bdcbbf46e4d400d5970547
leitegs <leitedasilvag@gmail.com>
2021-06-12 00:55:52 -0300

    refat: utilities controller-composer

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import { Request, Response, Router } from 'express'
+import getCompanyByCnpjController from './main/composers/get-company-by-cnpj-controller-composer'
 
 const routes = Router()
 
-routes.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello' })
-})
+routes.post('/getCompany', getCompanyByCnpjController.handle)
 
 export default routes
