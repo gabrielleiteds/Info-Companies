73c2597216f270f9c2f6814e916b597c26cba5af
leitegs <leitedasilvag@gmail.com>
2021-06-12 10:42:14 -0300

    refat: adjust composer

--- src/main/composers/get-company-by-cnpj-controller-composer.ts
+++ src/main/composers/get-company-by-cnpj-controller-composer.ts
@@ -1,11 +1,13 @@
 import GetCompanyByCnpj from '../../domain/usecase/company/get-company-by-cnpj'
+import ColaboratorRepository from '../../infra/repository/colaborator-repository'
 import CompanyRepository from '../../infra/repository/company-repository'
 import GetCompanyByCnpjController from '../../presentation/get-company-by-cnpj-controller'
 import ValidatorCnpj from '../../utils/validator-cnpj'
 
 const validatorCnpj = new ValidatorCnpj()
 const companyRepository = new CompanyRepository()
-const getCompanyByCnpjUseCase = new GetCompanyByCnpj(companyRepository, validatorCnpj)
+const colaboratorRepository = new ColaboratorRepository()
+const getCompanyByCnpjUseCase = new GetCompanyByCnpj(companyRepository, validatorCnpj, colaboratorRepository)
 const getCompanyByCnpjController = new GetCompanyByCnpjController(getCompanyByCnpjUseCase)
 
 export default getCompanyByCnpjController
