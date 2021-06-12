ed78d5b61dcc58ca82819c61e88635dc4feb4b38
leitegs <leitedasilvag@gmail.com>
2021-06-12 10:40:05 -0300

    refat: remove imports desnecessary

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { Router } from 'express'
 import getCompanyByCnpjController from './main/composers/get-company-by-cnpj-controller-composer'
 
 const routes = Router()
