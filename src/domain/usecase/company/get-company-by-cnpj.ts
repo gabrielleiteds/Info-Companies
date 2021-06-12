2833ee84e83649c25357fb34d87fd2c74577a2aa
leitegs <leitedasilvag@gmail.com>
2021-06-12 11:31:47 -0300

    feat: refat to using service brasil.io

--- src/domain/usecase/company/get-company-by-cnpj.ts
+++ src/domain/usecase/company/get-company-by-cnpj.ts
@@ -3,14 +3,18 @@ import ValidatorCnpj from '../../../utils/validator-cnpj'
 import axios from 'axios'
 import ColaboratorRepository from '../../../infra/repository/colaborator-repository'
 
+import BrasilIoService from '../../../infra/services/brasil-io-service'
+
 export default class GetCompanyByCnpj {
   private readonly companyRepository: CompanyRepository
   private readonly colaboratorRepository: ColaboratorRepository
+  private readonly brasilIoService: BrasilIoService
   private readonly validatorCnpj: ValidatorCnpj
-  constructor (companyRepository: CompanyRepository, validatorCnpj: ValidatorCnpj, colaboratorRepository: ColaboratorRepository) {
+  constructor (companyRepository: CompanyRepository, brasilIoService: BrasilIoService, validatorCnpj: ValidatorCnpj, colaboratorRepository: ColaboratorRepository) {
     this.companyRepository = companyRepository
     this.validatorCnpj = validatorCnpj
     this.colaboratorRepository = colaboratorRepository
+    this.brasilIoService = brasilIoService
   }
 
   async get (cnpj: String): Promise<any> {
@@ -21,20 +25,7 @@ export default class GetCompanyByCnpj {
     const findCompany = await this.companyRepository.getCompanyByCnpj(cnpj)
 
     if (!findCompany) {
-      const keyToken = process.env.TOKEN
-
-      const [companies, colaborators] = await Promise.all([
-        axios.get(`https://api.brasil.io/v1/dataset/socios-brasil/empresas/data?cnpj=${cnpj}`, {
-          headers: {
-            Authorization: `Token ${keyToken}`
-          }
-        }),
-        axios.get(`https://api.brasil.io/v1/dataset/socios-brasil/socios/data?cnpj=${cnpj}`, {
-          headers: {
-            Authorization: `Token ${keyToken}`
-          }
-        })
-      ])
+      const { companies, colaborators } = await this.brasilIoService.getCompanyAndColaborators(cnpj)
 
       const formatColaborators = colaborators.data.results.map(colaborator => {
         return {
