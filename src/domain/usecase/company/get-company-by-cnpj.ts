14c74ff28dafd460b0349e8ebaa1f7e52a0e6212
leitegs <leitedasilvag@gmail.com>
2021-06-12 10:41:00 -0300

    feat: implementing register to company and colaborator in my database

--- src/domain/usecase/company/get-company-by-cnpj.ts
+++ src/domain/usecase/company/get-company-by-cnpj.ts
@@ -1,13 +1,16 @@
-import fetch from 'cross-fetch'
 import CompanyRepository from '../../../infra/repository/company-repository'
 import ValidatorCnpj from '../../../utils/validator-cnpj'
+import axios from 'axios'
+import ColaboratorRepository from '../../../infra/repository/colaborator-repository'
 
 export default class GetCompanyByCnpj {
   private readonly companyRepository: CompanyRepository
+  private readonly colaboratorRepository: ColaboratorRepository
   private readonly validatorCnpj: ValidatorCnpj
-  constructor (companyRepository: CompanyRepository, validatorCnpj: ValidatorCnpj) {
+  constructor (companyRepository: CompanyRepository, validatorCnpj: ValidatorCnpj, colaboratorRepository: ColaboratorRepository) {
     this.companyRepository = companyRepository
     this.validatorCnpj = validatorCnpj
+    this.colaboratorRepository = colaboratorRepository
   }
 
   async get (cnpj: String): Promise<any> {
@@ -18,14 +21,42 @@ export default class GetCompanyByCnpj {
     const findCompany = await this.companyRepository.getCompanyByCnpj(cnpj)
 
     if (!findCompany) {
-      const response = await fetch(`https://api.brasil.io/v1/dataset/socios-brasil/empresas/data?cnpj=${cnpj}`, {
-        method: 'get',
-        headers: {
-          Authorization: `Token ${process.env.PORT}`
-        }
-      }).then(async result => result.json())
+      const keyToken = process.env.TOKEN
 
-      return response
+      const [companies, colaborators] = await Promise.all([
+        axios.get(`https://api.brasil.io/v1/dataset/socios-brasil/empresas/data?cnpj=${cnpj}`, {
+          headers: {
+            Authorization: `Token ${keyToken}`
+          }
+        }),
+        axios.get(`https://api.brasil.io/v1/dataset/socios-brasil/socios/data?cnpj=${cnpj}`, {
+          headers: {
+            Authorization: `Token ${keyToken}`
+          }
+        })
+      ])
+
+      const formatColaborators = colaborators.data.results.map(colaborator => {
+        return {
+          identity: colaborator.cpf_cnpj_socio,
+          name: colaborator.nome_socio,
+          qualification: colaborator.qualificacao_socio,
+          typePartner: colaborator.tipo_socio,
+          companyCnpj: colaborator.cnpj
+        }
+      })
+      // evita de bater duas vezes no banco para criar duas coisas diferentes
+      const [company] = await Promise.all([
+        this.companyRepository.create({
+          cnpj: companies.data.results[0].cnpj,
+          uf: companies.data.results[0].cnpj.uf,
+          reasonSocial: companies.data.results[0].razao_social,
+          colaborators: formatColaborators
+        }),
+        this.colaboratorRepository.createColaborators(formatColaborators)
+      ])
+
+      return { companies: company }
     }
 
     return findCompany
