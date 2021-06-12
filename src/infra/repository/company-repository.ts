1747fff34d0c90e4ec50ef69996d057f087e98d3
leitegs <leitedasilvag@gmail.com>
2021-06-12 10:41:59 -0300

    feat: implementing function to create company

--- src/infra/repository/company-repository.ts
+++ src/infra/repository/company-repository.ts
@@ -1,7 +1,11 @@
 import companySchema from '../helpers/mongoose/schemas/company-schema'
-
+import ICompany from '../../domain/interface/interface-company'
 export default class CompanyRepository {
   async getCompanyByCnpj (cnpj: String) {
-    return await companySchema.findOne({ cnpj }).populate('colaborators')
+    return await companySchema.findOne({ cnpj })
+  }
+
+  async create (company: ICompany) {
+    return await companySchema.create(company)
   }
 }
