8d0237ed3b970515c5a1b3b7ccc8907e15af2286
leitegs <leitedasilvag@gmail.com>
2021-06-12 01:02:09 -0300

    feat: Implementing controller for get-company-by-cnpj

---
+++ src/presentation/get-company-by-cnpj-controller.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from 'express'
+import GetCompanyByCnpj from '../domain/usecase/company/get-company-by-cnpj'
+
+export default class GetCompanyByCnpjController {
+  private readonly getCompanyByCnpjUseCase: GetCompanyByCnpj
+  constructor (getCompanyByCnpjUseCase: GetCompanyByCnpj) {
+    this.getCompanyByCnpjUseCase = getCompanyByCnpjUseCase
+    this.handle = this.handle.bind(this)
+  }
+
+  async handle (req: Request, res: Response): Promise<any> {
+    const { cnpj } = req.body
+
+    const response = await this.getCompanyByCnpjUseCase.get(cnpj)
+
+    return res.status(200).send(response)
+  }
+}
